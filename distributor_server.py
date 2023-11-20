329b0af41dc31f199e1addc016a8523fa2d014a7
onecheck <1548767361@qq.com>
2023-11-20 22:24:29 +0800

    update AutoEdge v0.0.2

---
+++ distributor_server.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, BackgroundTasks
+
+from fastapi.routing import APIRoute
+from starlette.responses import JSONResponse
+from starlette.requests import Request
+from fastapi.middleware.cors import CORSMiddleware
+
+
+class DistributorServer:
+    def __init__(self):
+        pass
+
