4b290dedf11daf6b4cf0161b181097ca7e8a8505
onecheck <1548767361@qq.com>
2023-11-21 15:16:11 +0800

    update AutoEdge v0.0.3

--- distributor_server.py
+++ distributor_server.py
@@ -8,5 +8,26 @@ from fastapi.middleware.cors import CORSMiddleware
 
 class DistributorServer:
     def __init__(self):
+        self.app = FastAPI(routes=[
+            APIRoute('/predict',
+                     self.deal_response,
+                     response_class=JSONResponse,
+                     methods=['POST']
+
+                     ),
+        ], log_level='trace', timeout=6000)
+
+        self.app.add_middleware(
+            CORSMiddleware, allow_origins=["*"], allow_credentials=True,
+            allow_methods=["*"], allow_headers=["*"],
+        )
+
+    def distribute_data(self, data):
+
+        # TODO: post data to aggregator
         pass
 
+    async def deal_response(self, request: Request, backtask: BackgroundTasks):
+        data = await request.json()
+        backtask.add_task(self.distribute_data, data)
+        return {'msg': 'data send success!'}
